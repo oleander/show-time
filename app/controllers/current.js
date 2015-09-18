8bce58fa7f1c2f03d1e6741c7bc17e4c262d9515
Linus Oleander <linus@oleander.nu>
2015-09-19 00:19:35 +0200

    Remove 'last update' status

--- app/controllers/current.js
+++ app/controllers/current.js
@@ -1,9 +1,6 @@
 export default Ember.Controller.extend({
   showAll: true,
   needs: ["application", "episodes"],
-  updatedAt: function() {
-    return this.get('controllers.application.updatedAt');
-  }.property('controllers.application.updatedAt'),
   isUpdating: function() {
     return this.get('controllers.application.isUpdating');
   }.property('controllers.application.isUpdating'),
