6a7c12e836a99ddc01e27adf4a1f8bb4ebc52d6c
Linus Oleander <linus@oleander.nu>
2015-09-26 22:17:23 +0200

    Clean up

--- app/routes/user/index.js
+++ app/routes/user/index.js
@@ -2,15 +2,10 @@ export default Ember.Route.extend({
   model: function() {
     return this.store.query("episode", { seen: false, removed: false });
   },
-  setupController: function() {
-    // this.controllerFor("user").set("model", this.model);
-  },
-
   renderTemplate: function(){
     this.render();
-    this.render('user.menu', {
-      outlet: "menu"//,
-      // controller: 'user'
+    this.render("user.menu", {
+      outlet: "menu"
     });
   }
 });
--- app/routes/user/removed.js
+++ app/routes/user/removed.js
@@ -1,8 +1,5 @@
 export default Ember.Route.extend({
   model: function() {
     return this.store.query("episode", { removed: true });
-  },
-  setupController: function() {
-    this.controllerFor("user").set("model", this.model);
   }
 });
