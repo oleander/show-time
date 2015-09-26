69247b9df1bc2758ec554abdf49d5831a3bb4df7
Linus Oleander <linus@oleander.nu>
2015-09-27 01:54:50 +0200

    Move most assets to pipeline

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,4 +1,4 @@
-var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var EmberApp = require("ember-cli/lib/broccoli/ember-app");
 
 module.exports = function(defaults) {
     var app = new EmberApp(defaults, {
@@ -10,12 +10,16 @@ module.exports = function(defaults) {
       }
     });
 
-    app.import('bower_components/ember-localstorage-adapter/localstorage_adapter.js');
-    app.import('vendor/app.js');
-    app.import('vendor/AdminLTE.css');
-    app.import('vendor/_all-skins.css');
-    app.import('vendor/vegas.css');
-    app.import('vendor/vegas.js');
+    app.import("bower_components/ember-localstorage-adapter/localstorage_adapter.js");
+    app.import("bower_components/bootstrap/dist/css/bootstrap-theme.css");
+    app.import("bower_components/font-awesome/css/font-awesome.css");
+    app.import("bower_components/bootstrap/dist/js/bootstrap.js");
+    app.import("vendor/app.js");
+    app.import("vendor/AdminLTE.css");
+    app.import("vendor/_all-skins.css");
+    app.import("vendor/vegas.css");
+
+    app.import("vendor/vegas.js");
 
     return app.toTree();
 };
