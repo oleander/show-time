de52d0df5fbc98b201b266f530a543bd9621f2cd
Linus Oleander <linus@oleander.nu>
2015-10-11 20:30:38 +0200

    Add style to player

--- app/controllers/user/play.js
+++ app/controllers/user/play.js
@@ -2,11 +2,11 @@ var peerflix = nRequire("peerflix");
 
 export default Ember.Controller.extend({
   engine: null,
+  player: null,
+  isLoading: true,
   actions: {
     exit: function(){
-      var engine = this.get("engine");
-      if(engine) { console.info("kill engine"); engine.destroy(); }
       this.transitionToRoute("user.index");
     }
   }
-})
+});
--- app/router.js
+++ app/router.js
@@ -7,12 +7,10 @@ var Router = Ember.Router.extend({
 
 export default Router.map(function() {
   this.route("user", function() {
-    this.route("index", function() {
-      this.route("play", { path: ":id/play" });
-    });
-
+    this.route("index");
     this.route("removed");
     this.route("seen");
+    this.route("play", { path: ":id/play" });
   });
 
   this.route("login");
