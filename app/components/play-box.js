00812ba0c1efaa44235a377d44f7a94cf721bb2b
Linus Oleander <linus@oleander.nu>
2015-10-12 01:16:27 +0200

    Move download logic into component

--- app/components/play-box.js
+++ app/components/play-box.js
@@ -1,43 +1,65 @@
 var wjs = nRequire("wcjs-player");
+var peerflix = nRequire("peerflix");
 
 export default Ember.Component.extend({
   classNames: ["player-box"],
   didInsertElement: function() {
+    console.info("didInsertElement");
     var self = this;
-    var id = this.$().attr("id");
+    var engine = peerflix(this.get("magnet"));
+    engine.server.on("listening", function() {
+      console.info("loaded url")
+      self.hasLoadedUrl(
+        "http://localhost:" + 
+        engine.server.address().port + 
+        "/"
+      );
+    });
+  },
+  hasLoadedUrl: function(url){
+    console.info("start player")
+    var self = this;
     var player = new wjs("#player").addPlayer({ autoplay: true });
-    player.addPlaylist(this.get("url"));
+    player.addPlaylist(url);
     player.ui(true);
     player.video(true);
     player.volume(0);
 
+    this.set("player", player);
+
+    player.onFrameSetup(function(){
+      self.onFirstFrame();
+    });
+  },
+  willDestroyElement: function(){
+    console.info("stop!")
+    var player = this.get("player");
+    if(player) { player.stop(); }
+
+    var engine = this.get("engine");
+    if(engine) { engine.destroy(); }
+  },
+  onFirstFrame: function(){
     var $close = this.$().find("#close");
-    var $el = this.$().find(".wcp-toolbar");
+    var $toolbar = this.$().find(".wcp-toolbar");
+    var self = this;
+
+    console.info("onFrameSetup", $close);
 
     $close.click(function(){
       console.info("close...")
       self.sendAction("close");
     });
 
-    // define a new observer
-    var obs = new MutationObserver(function(mutations,b){
-      if($el.is(":visible")) {
+    new MutationObserver(function(mutations,b){
+      if($toolbar.is(":visible")) {
         $close.removeClass("hide");
       } else {
         $close.addClass("hide");
       }
-    });
-
-    obs.observe($el.get(0), {
+    }).observe($toolbar.get(0), {
       attributes: true,
       subtree: false
     });
-
-
-    this.set("player", player);
-  },
-  willDestroyElement: function(){
-    console.info("stop!")
-    this.get("player").stop();
   }
 })
--- app/routes/user/play.js
+++ app/routes/user/play.js
@@ -3,24 +3,5 @@ var peerflix = nRequire("peerflix");
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.find("episode", params.id);
-  },
-  deactivate: function() {
-    var controller = this.controllerFor("user.play");
-
-    var engine = controller.get("engine");
-    if(engine) { console.info("remove"); engine.destroy(); }
-  
-    var player = controller.get("player");
-    if(player) { console.info("remove player"); player.stop(); }
-  },
-  afterModel: function(episode){
-    var self = this;
-    var controller = this.controllerFor("user.play");
-    var engine = peerflix(episode.get("magnet"));
-    controller.set("engine", engine);
-    engine.server.on("listening", function() {
-      var url = "http://localhost:" + engine.server.address().port + "/";
-      controller.set("url", url);
-    });
   }
 });
