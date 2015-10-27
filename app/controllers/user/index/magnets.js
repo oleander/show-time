7ce44b4af63ec2adfc8240bd4bf62be8704b4e64
Linus Oleander <linus@oleander.nu>
2015-10-28 00:12:28 +0100

    Add play button to magnet list view

--- app/controllers/user/index/magnets.js
+++ app/controllers/user/index/magnets.js
@@ -2,6 +2,12 @@ export default Ember.Controller.extend({
   actions: {
     back: function(){
       this.transitionToRoute("user.index");
+    },
+    play: function(magnet) {
+      this.transitionToRoute("play",
+        this.get("episode").get("id"),
+        magnet.get("id")
+      );
     }
   }
 });
