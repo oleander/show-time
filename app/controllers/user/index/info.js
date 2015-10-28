0365fc791c12ccc2a0d140037ae9eec9871811df
Linus Oleander <linus@oleander.nu>
2015-10-28 01:06:37 +0100

    Fix info bar

--- app/controllers/user/index/info.js
+++ app/controllers/user/index/info.js
@@ -2,18 +2,23 @@ var shell = nRequire("shell");
 
 export default Ember.Controller.extend({
   indexController: Ember.inject.controller("user.index"),
+  close: function(){
+    this.transitionToRoute("user.index");
+  },
   actions: {
     remove: function() {
       var should = confirm("Are you sure?");
       if(!should) { return; }
       this.get("indexController").model.removeObject(this.model);
       this.model.isRemoved();
+      this.close();
     },
     seen: function() {
       var should = confirm("Are you sure?");
       if(!should) { return; }
       this.get("indexController").model.removeObject(this.model)
       this.model.hasSeen();
+      this.close();
     },
     reload: function() {
       this.model.loading();
@@ -23,7 +28,7 @@ export default Ember.Controller.extend({
       shell.openExternal(this.model.get("magnet"));
     },
     closeInfoBar: function(){
-      this.transitionToRoute("user.index");
+      this.close();
     },
     magnets: function(){
       this.transitionToRoute("user.index.magnets", this.model.get("id"));
