c59a2aa353cb90f1732eaf156c8539d79fd5fb13
Linus Oleander <linus@oleander.nu>
2015-11-03 01:56:14 +0100

    Add tpb as torrent endpoint

--- app/lib/getTorrentFromEpisode.js
+++ app/lib/getTorrentFromEpisode.js
@@ -1,61 +1,62 @@
 import getJSON from "./getJSON";
 import User from "./user";
+var tpb = nRequire("thepiratebay");
 
-var search = function(query) {
-  return new Promise(function(resolve, reject) {
-    var params = {
-      qs: {
-        q: query,
-        field: "seeders",
-        order: "desc",
-        verified: "1"
-      },
-      url: "https://kat.cr/json.php"
+var cleanUp = function(torrents) {
+  return torrents.map(function(torrent){
+    return {
+      href: "magnet:?xt=urn:btih:" + torrent.hash + "&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&dn=" + encodeURIComponent(torrent.title),
+      title: torrent.title,
+      seeders: torrent.seeds
     };
+  })
+}
 
-    getJSON(params).then(function(data){
-      if (!data.list.length) {
-        reject("No torrent matches when searching for '" + query + "'");
-      } else {
-        var torrents = data.list;
-        if(torrents.length) {
-          resolve(torrents.filterBy("verified").sortBy("seeds").reverse().slice(0, 10));
-        } else {
-          reject("No torrents found");
-        }
-      }
-    }).catch(reject);
+var search1 = function(query) {
+  var params = {
+    qs: {
+      q: query,
+      field: "seeders",
+      order: "desc",
+      verified: "1"
+    },
+    url: "https://kat.cr/json.php"
+  };
+
+  return getJSON(params).then(function(data){
+    return resolve(cleanUp(data.list.filterBy("verified").sortBy("seeds").reverse()));
   });
 };
 
-export default function(episode) {
-  return new Promise(function(resolve, reject) {
-    var query1 = episode.get("show") + " " + episode.get("what");
-    var query2 = episode.get("show") + " " + episode.get("title");
-
-    var resultToMagnet = function(torrents) {
-      return torrents.map(function(torrent){
-        return {
-          // TODO: Make this shorter
-          href: "magnet:?xt=urn:btih:" + torrent.hash + "&tr=udp%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337&dn=" + encodeURIComponent(torrent.title),
-          title: torrent.title,
-          seeders: torrent.seeds
-        };
-      });
-    };
-
-    // First, search for show and episode number
-    // Example: Mythbusters s16e06
-    search(query1).then(function(result) {
-      resolve(resultToMagnet(result));
-    }, function(err) {
-      // On no-match, search for show and episode title
-      // Example: Mythbusters Unfinished Business
-      search(query2).then(function(result) {
-        resolve(resultToMagnet(result));
-      }, function(err) {
-        reject(err);
-      });
+var search2 = function(query){
+  return tpb.search(query, { category: "200", orderBy: "7" }).then(function(results){
+    return results.map(function(result){
+      return {
+        href: result.magnetLink,
+        title: result.name,
+        seeders: parseInt(result.seeders, 10)
+      };
     });
+  });
+}
+
+var search = function(query) {
+  return new Promise(function(resolve, reject) {
+    return search1(query).then(function(results){
+      results.length ? resolve(results.slice(0, 10)) : reject("Nothing found for search1");
+    }).catch(function(err){
+      return search2(query);
+    }).then(function(results){
+      results.length ? resolve(results.slice(0, 10)) : reject("Nothing found for search2");
+    });
+  });
+}
+
+export default function(episode) {
+  var show = episode.get("show");
+  var query1 = show + " " + episode.get("what");
+  var query2 = show + " " + episode.get("title");
+  return search(query1).catch(function(err) {
+    return search(query2);
   });
 }
