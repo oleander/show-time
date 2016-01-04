a70a4fa5477e3cd4d752247a15221aae485851bb
Linus Oleander <linus@oleander.nu>
2016-01-04 20:08:42 +0100

    Fix reload button

--- app/router.js
+++ app/router.js
@@ -8,7 +8,6 @@ var Router = Ember.Router.extend({
 export default Router.map(function() {
   this.route("user", function() {
     this.route("index", function(){
-      this.route("info", { path: ":id/info" });
       this.route("magnets", { path: ":id/magnets" });
     });
     this.route("removed");
